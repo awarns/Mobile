946da41374d168f75b4213d5de07c4bfeaa72843
awarns <itqa1@MAC14958.local>
2013-08-28 22:26:58 -0400

    Merge branch 'development'

--- MobileAutomationSuite/features/support/env.rb
+++ MobileAutomationSuite/features/support/env.rb
@@ -9,7 +9,7 @@ require_rel '/screens'
 
 World(Furter::Navigation)
 
-APP_BUNDLE_PATH = File.expand_path( '/Users/awarns/app/31gifts/iOS/Frank/frankified_build/Frankified.app', __FILE__ )
+APP_BUNDLE_PATH = File.expand_path( '/Users/itqa1/app/31gifts/iOS/Frank/frankified_build/Frankified.app', __FILE__ )
 
 Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 
