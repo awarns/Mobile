f894e4e947f9c96f9794004a19c6247af7c3ff6a
awarns <itqa1@MAC14958.local>
2013-10-08 15:44:05 -0400

    Fix RakeFile

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,10 +2,5 @@ Given(/^I Login to the Mobile App$/) do
 
 
   on(LoginScreen).login
-<<<<<<< HEAD:MobileAutomationSuite/features/step_definitions/login_steps.rb
-
-=======
->>>>>>> development:features/step_definitions/login_steps.rb
-
 
 end
